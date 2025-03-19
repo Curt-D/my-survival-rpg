ca67402758676f0798d7542d357f41e70ed0cae4
Curt-D <cgdimundo@gmail.com>
2025-03-19 11:57:29 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FullSurvivalRPG from "./FullSurvivalRPG";
+
+function App() {
+  return (
+    <div className="App">
+      <FullSurvivalRPG />
+    </div>
+  );
+}
+
+export default App;
